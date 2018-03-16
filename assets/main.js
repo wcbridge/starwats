43227b76f860c108b7807ea371262637910eaf61
wcbridge <33330345+wcbridge@users.noreply.github.com>
2018-03-16 14:20:54 -0400

    starting

---
+++ assets/main.js
@@ -0,0 +1,24 @@
+
+var luke = {
+ "hp" : 100,    
+ "ap" : 10,
+ "cap": 12,
+}
+
+var itchy = {
+    "hp": 150,
+    "ap": 5,
+    "cap": 10,
+}
+
+var lumpy = {
+    "hp": 50,
+    "ap": 30,
+    "cap": 40,
+}
+
+var george = {
+    "hp": 70,
+    "ap": 12,
+    "cap": 25,
+}
